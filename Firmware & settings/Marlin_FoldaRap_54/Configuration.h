5be02af3a96fa10c35dcce36f4d073e65be4ba90
Emmanuel Gilloz <emmanuel.gilloz@gmail.com>
2013-09-29 09:23:46 +0200

    m

--- Firmware & settings/Marlin_FoldaRap_54/Configuration.h
+++ Firmware & settings/Marlin_FoldaRap_54/Configuration.h
@@ -257,7 +257,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
-#define HOMING_FEEDRATE {30*60, 30*60, 2*60, 0}  // set the homing speeds (mm/min)
+#define HOMING_FEEDRATE {1800, 1800, 115, 0}  // set the homing speeds (mm/min)
 
 // default settings 
 
